42d70f720e1da29fe4eba65c796472c996edc144
lvgraham <liavibanez@gmail.com>
2020-07-23 22:00:44 -0500

    adding database, config, and orm

---
+++ config/orm.js
@@ -0,0 +1,54 @@
+const connection = require('./connection');
+
+const orm = {
+    all: function(tableInput, cb) {
+        let queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    create: function(table, cols, vals, cb) {
+        let queryString = 'INSERT INTO' + table;
+
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    update: function(table, objColVals, condition, cb) {
+        let queryString = 'UPDATE' + table;
+
+        queryString += ' SET ';
+        queryString += objToSql(objColVals);
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+}
+
+//export the orm object for the model
+module.exports = orm;
