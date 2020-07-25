a9f9e8618d12662f0d4c85cad10425030cd457ad
lvgraham <liavibanez@gmail.com>
2020-07-25 16:45:47 -0500

    fixing typo

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ const mysql = require("mysql");
 // });
 
 if (process.env.JAWSDB_URL){
-  connection = myswl.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
