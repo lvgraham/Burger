c459863a993789424c1e6be2971d6c02d0ca74be
lvgraham <liavibanez@gmail.com>
2020-07-25 16:43:12 -0500

    testing new connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: 'password',
-  database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: 'password',
+//   database: "burgers_db"
+// });
 
 if (process.env.JAWSDB_URL){
   connection = myswl.createConnection(process.env.JAWSDB_URL);
